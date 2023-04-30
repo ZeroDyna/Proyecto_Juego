cded1b8d679c823f63bfa4bd84da1ad360179c9e
RenatoUCSP1 <renato.corrales@ucsp.edu.pe>
2023-04-30 17:46:57 -0500

    musica no lo pude ejecutar asi que solo lo subo de manera de prueba

--- juego/main.cpp
+++ juego/main.cpp
@@ -1,12 +1,19 @@
 #include <SFML/Graphics.hpp>
 #include "Personaje.hpp"
+#include "SFML/Audio.hpp"
 
 int main() {
     sf::RenderWindow ventana(sf::VideoMode(1800, 920), "Mi juego");
     ventana.setFramerateLimit(60);
 
     Personaje personaje(5);
+    sf::Music music;
+    if (!music.openFromFile(revival.ogg)){
+        std::cout << "ERROR" << std::endl;
 
+    }
+    music.play();
+    
     while (ventana.isOpen()) {
         sf::Event evento;
         while (ventana.pollEvent(evento)) {
